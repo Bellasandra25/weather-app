9191c517e037fd311d2dedf2c2a14290719ea905
Sandra Odubela <odubelaoluwadamilola@gmail.com>
2025-01-09 15:38:55 +0000

    finale

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-
+import backgroundImg from './assets/Background-image.jpeg';  
 const App = () => {
   const [city, setCity] = useState('')
   const [weather, setWeather] = useState(null)
@@ -24,30 +24,42 @@ const App = () => {
   };
 
   return (
-    <div className='container flex flex-col justify-center items-center'>
-      <h1>Weather App</h1>
-      <div className='container-top mt-20 flex flex-col justify-between items-center'>
+    <div className='container flex flex-col bg-cover bg-center min-h-screen' style={{ backgroundImage: `url(${backgroundImg})` }}>
+      <h1 className='items-center grid'>Weather App</h1>
+      <div className='container-top mx-[8rem]  flex justify-around items-center bg-[rgba(151, 85, 85, 0.5)] rounded-full p-4 rounded-lg'>
         <input 
+        className='p-4 rounded-full outline-none bg-neutral-50/50 text-black flex justify-center items-center'
           value={city}
           onChange={(e) => setCity(e.target.value)}
           type="text"
           name="city" 
           placeholder='Search Location'
         />
-        <button onClick={handleSubmit} type="search">Search</button>
+        <button 
+        className=' flex justify-center items-center rounded-full p-2 outline-none bg-white text-black hover:bg-black hover:text-white'
+        onClick={handleSubmit} type="search">Search</button>
       </div>
 
       {loading ? (
         <p>Loading.....</p>
       ) : weather ? (
         <>
-          <div className='container-bottom'>
+          <div className='container-bottom flex space-y-5 justify-center items-center mt-20'>
             <h2>City Name: {weather.location.name}</h2>
-            <h3>Temperature: {weather.current.temp_c}°C</h3>
           </div>
-          <div className="border rounded-lg p-4 m-4 bg-[rgba(228, 213, 213, 0.1)]">
+          <div className="bg-neutral-50/75 rounded-lg justify-between flex p-4 m-[8rem] bg-[rgba(228, 213, 213, 0.1)]">
+            <div>
             <h3>Wind Speed</h3>
             <p>{weather.current.wind_kph} km/h</p>
+            </div>
+            <div>
+            <h3>Temperature</h3>
+            <p>{weather.current.temp_c}°C</p>
+            </div>
+            <div>
+            <h3>Wind Speed</h3>
+            <p>{weather.current.wind_kph} km/h</p>
+            </div>
           </div>
         </>
       ) : null}
